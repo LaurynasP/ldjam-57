2466cfafad70a11164639a5b9f55c3c37b7feab7
Vilmantas <{ID}+{username}@users.noreply.github.com>
2025-04-05 13:17:29 +0300

    Add furnace recipes and recipe helper methods

--- features/stations/furnace/furnace.gd
+++ features/stations/furnace/furnace.gd
@@ -4,5 +4,24 @@ extends Station
 @export var progress: int
 @export var recipes: Array[Recipe]
 
+var loaded_recipes: Dictionary[String, Recipe]
+var available_recipes: Dictionary[String, Recipe]
+
+var added_resources: Array[Item] = []
+
+signal on_resource_added(resource: String)
+
 func _ready() -> void:
+	loaded_recipes = RecipeManager.get_recipes(recipes)
+	available_recipes = loaded_recipes
+
+func add_item(item: String) -> bool:
+	if (RecipeManager.is_resource_in_recipes(item, loaded_recipes.values())):
+		available_recipes = RecipeManager.get_related_recipes(item, available_recipes.values())
+		on_resource_added.emit(item)
+		return true
 	
+	return false
+	
+func do_processing():
+	pass
--- managers/recipe_manager/recipe_manager.gd
+++ managers/recipe_manager/recipe_manager.gd
@@ -7,3 +7,28 @@ func _ready() -> void:
 	
 	for recipe in r:
 		recipes[recipe.name] = recipe
+
+func get_recipes(searchables: Array[Recipe]) -> Dictionary[String, Recipe]:
+	var result: Dictionary[String, Recipe] = {}
+	for item in searchables:
+		result[item.name] = recipes[item.name]
+		
+	return result
+	
+func is_resource_in_recipes(resource: String, available_recipes: Array[Recipe]) -> bool:
+	for recipe in available_recipes:
+		if is_resource_in_recipe(resource, recipe):
+			return true
+		
+	return false
+	
+func is_resource_in_recipe(resource: String, recipe: Recipe) -> bool:
+	return recipe.ingredients.any(func(i: Item): return i.name == resource)
+
+func get_related_recipes(resource: String, available_recipes: Array[Recipe]) -> Dictionary[String, Recipe]:
+	var result: Dictionary[String, Recipe] = {}
+	for recipe in available_recipes:
+		if is_resource_in_recipe(resource, recipe):
+			result[recipe.name] = recipe
+		
+	return result
