ccbccdf13e9c01ea64bff9d5a0b5b2c8fda3a6ce
Vilmantas <15175886+vilmantas@users.noreply.github.com>
2025-04-06 21:18:37 +0300

    Add furnace UI spawn

--- features/stations/station.gd
+++ features/stations/station.gd
@@ -13,7 +13,13 @@ signal on_resource_added(resource: Item)
 func _ready() -> void:
 	var ui_instance = load("res://features/stations/StationUIBillboard.tscn").instantiate()
 	ui = ui_instance
-	add_child(ui_instance)
+	
+	var target = find_child('ui_spawn')
+	
+	if target != null:
+		target.add_child(ui_instance)
+	else:
+		add_child(ui_instance)
 
 func add_item(item: Item) -> bool:
 	if inventory_space <= inventory.size() or (not allow_duplicates and ItemManager.is_item_already_in_inventory(item, inventory)):
