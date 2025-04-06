197d91bbc849bd00d7a8babbf2ae98c71f84d369
Vilmantas <15175886+vilmantas@users.noreply.github.com>
2025-04-06 10:43:59 +0300

    Add gameplay camera

--- features/gameplay/gameplay.gd
+++ features/gameplay/gameplay.gd
@@ -3,5 +3,16 @@ extends Node
 
 var players: Dictionary[int, Player] = {}
 
+@onready var camera := $PhantomCamera3D
+
 func _ready() -> void:
 	GameManager.current_gameplay = self
+
+func _add_player(device_id: int, player: Player) -> void:
+	players[device_id] = player
+	var something: Array[Node3D] = []
+	
+	for p in players.values():
+		something.append(p as Node3D)
+	
+	camera.set_follow_targets(something)
